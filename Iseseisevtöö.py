0fce9dbee5bf1144575e98a2f545ea68fb82c92b
ArtjomVinogradov <98739429+ArtjomVinogradov@users.noreply.github.com>
2022-03-14 14:10:04 +0200

    Add files via upload

--- Iseseisevtöö.py
+++ Iseseisevtöö.py
@@ -5,11 +5,18 @@ aken = turtle.Screen()
 aken.setup(300,300)
 
 tri = turtle.Turtle()
- 
-for i in range(6):
-    
-    tri.left(144)
-    tri.forward(100)
- 
 
-board = turtle.Turtle()
+for i in range(5):
+    tri.left(360/5)
+    tri.forward(50)
+    tri.left(120)
+    tri.forward(100)
+    tri.left(120)
+        
+    tri.forward(100)
+    tri.left(120)
+    tri.forward(50)
+    
+    
+
+turtle.exitonclick()
