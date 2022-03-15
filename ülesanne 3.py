37643d247594084be35a2bdb423c0274b06a5628
ArtjomVinogradov <98739429+ArtjomVinogradov@users.noreply.github.com>
2022-03-15 09:40:11 +0200

    Add files via upload

---
+++ ülesanne 3.py
@@ -0,0 +1,8 @@
+from tkinter import *
+
+aken = Tk()
+aken.title("Artjom")
+tekst = Label(aken, text="Nimi: Artjom\n Rühm:IT21\n 2022",foreground="red", pady=10, padx=30, background="black", font="Tahoma 16 bold italic").pack()
+tekst.pack()
+
+aken.mainloop()
