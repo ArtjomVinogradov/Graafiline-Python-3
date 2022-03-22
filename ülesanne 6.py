6f5652a7be095d6caf8ef7fe279c363d65b62cbd
ArtjomVinogradov <98739429+ArtjomVinogradov@users.noreply.github.com>
2022-03-22 14:03:02 +0200

    Add files via upload

---
+++ ülesanne 6.py
@@ -0,0 +1,26 @@
+from tkinter import *
+#Artjom Vinogradov
+#IT-21
+#22.03.22
+#ülesanne 6
+
+
+
+
+
+#akna seaded
+aken = Tk()
+aken.title('Joonistamine')
+
+
+#lõuendi loomine
+louend = Canvas(aken, width=1000, height=1000)
+louend.pack()
+
+#kujundite loomine
+louend.create_rectangle(0, 0, 350, 100, fill='#1296F5', outline='#993333', width=0)
+
+louend.create_rectangle
+
+
+aken.mainloop()
