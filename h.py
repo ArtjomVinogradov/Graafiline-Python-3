0bf0fd7bf6a5db6ef5c13c594f0c64f44ef74a3f
ArtjomVinogradov <98739429+ArtjomVinogradov@users.noreply.github.com>
2022-03-28 11:46:30 +0300

    Add files via upload

---
+++ h.py
@@ -0,0 +1,27 @@
+#Mäng
+#Martin Pettai, Artjom Vinogradov, Jass Õunapuu
+#IT21
+
+print("Mäng")
+
+tehe=int(input('You want to play litle boy?\n 1) YES\n 2) NO\n'))
+
+if tehe==1:
+   print("Oh man Thats very nice")
+   
+elif tehe==2:
+    print("Man I dont care you dont have a choise")
+    
+    
+print('I will ask you a few questions and life of Kalevi Poeg will depend on the correct answer')
+
+first=int(input('First question:3 boys from the village, want to fight with Kalevi Poeg, what are you going to do?\n 1)run away\n 2)fight back with a hug from the left\n 3)answer verbally\n'))
+
+if first==1:
+    print('you barely managed to escape but you pissed your pants')
+    
+elif first==2:
+    print('You hit one with such force that he most likely died, and the others run away and you were left unharmed')
+
+elif first==3:
+    print('
